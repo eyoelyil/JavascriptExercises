b233d7f3a2dcda94f5f52317d42c4dc00ff7301e
eyoelyil <eyoelyil22@gmail.com>
2019-10-01 20:55:39 +0300

    first commit

---
+++ palindrome.js
@@ -0,0 +1,10 @@
+//Palindromes are words that form the same word if it is reversed.
+//Given a string, return true id the string is a palindrome.
+
+function palindrome(str) {
+    const reversed = str.split('').reverse('').join('');
+    return str === reversed
+        
+}
+
+console.log(palindrome('woooooow'))

5db5c87f829b92ce0a1a04b9172782051cf1d41f
eyoelyil <eyoelyil22@gmail.com>
2019-10-03 22:09:13 +0300

    update

--- reversestring.js
+++ reversestring.js
@@ -5,3 +5,17 @@ function reverse(str) {
 }
 
 console.log(reverse('hello'))
+
+
+// without reverse() method or any other methods. Only using for loop
+
+function reverseStr(str) {
+   let reversed = '';
+    
+   for(let i = str.length - 1; i >= 0; i--) {
+      reversed = reversed + str[i]
+   }
+   return reversed;
+}
+
+console.log(reverseStr('hello world'))

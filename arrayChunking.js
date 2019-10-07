52630f139e9219211e43698f2c10db0c8a085a2d
eyoelyil <eyoelyil22@gmail.com>
2019-10-07 16:13:10 +0300

    update

--- arrayChunking.js
+++ arrayChunking.js
@@ -1,6 +1,11 @@
-// Given an array and chunk size, divide the array into many subarrays where each subarray is of length size
-//Steps, create empty array to hold chunks called 'chunkde', for each element in the 'unchunked' array - retrieve the last element in 'chunkec',
-// if last element does not exist, or if its length is equal to chunk size, push a new chunk into 'chunked' with current element, else add the current element into the chunk.
+/* Given an array and chunk size, divide the array into many subarrays where each subarray is of length size
+Step-1, create empty array to hold chunks called 'chunked'
+Step-2, for each element in the 'unchunked' array 
+Step-3, retrieve the last element in 'chunked'
+Step-4, if last element does not exist, or if its length is equal to chunk size,
+        push a new chunk into 'chunked' with current element, 
+        else add the current element into the chunk. 
+*/
 
 
 //Soluntion 1
@@ -19,7 +24,7 @@
 }
  */
 
- //
+ // create empty 'chunked' array, create 'index' start at 0, while index is less than array.length, push a slice of length 'size' from 'array' into 'chunked', add 'size' to 'index'.
 function chunk(array, size) {
     const chunked = [];
     let index = 0;

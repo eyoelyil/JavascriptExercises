9c4a52acbc4711554b0c2103da67784b1fedcaef
eyoelyil <47233583+eyoelyil@users.noreply.github.com>
2019-10-25 23:43:19 +0300

    commit

---
+++ longestStrings.js
@@ -0,0 +1,18 @@
+function longestString(arr) {
+    let longestLength = 0;
+    const longestWords = [];
+
+    arr.forEach(word => {
+        longestLength = longestLength < word.length ? word.length : longestLength;
+    });
+
+    arr.forEach(word => {
+        if(word.length === longestLength) {
+            longestWords.push(word);
+        }
+    })
+
+    return longestWords;
+}
+
+console.log(longestString(["abcd", "abc", "abd", "defg"]))
